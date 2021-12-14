70cf64a6a7635c329cdbccf32ac4093fd868df6c
Rene-Zaltbommel <91453184+Rene-Zaltbommel@users.noreply.github.com>
2021-12-14 14:07:25 +0100

    Add files via upload

---
+++ derdeJava/index.js
@@ -0,0 +1,25 @@
+const age= 19;
+
+if (age > 18){
+    console.log( 'you can go in');
+} else if ( age == 18){
+    console.log( 'you can go in');
+}else{ 
+    console.log( 'you need te be 18 so i am sorry but i can not let you in');
+}
+
+const isFemale= false;
+
+if (isFemale){
+    console.log( 'yeahh lets have a girls night');
+} else{
+    console.log( 'please note you are not allowed in, it is only for girls this evening');
+}
+
+const driveSatus= 'bob';
+
+if (driveSatus){
+    console.log( 'yes you may drive');
+} else{
+    console.log( 'please do not drive!!!! it is dangerous with alcohol');
+}
